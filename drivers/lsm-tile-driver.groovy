5b962156e956cc033736398368538af86f89da26
Warlock-Weary <88459215+Warlock-Weary@users.noreply.github.com>
2025-09-28 11:30:16 -0500

    Update lsm-tile-driver.groovy

--- drivers/lsm-tile-driver.groovy
+++ drivers/lsm-tile-driver.groovy
@@ -1,1 +1,18 @@
-tile-driver
+/**
+ * LSM Tile Device v20.0
+ * Virtual child device for Light Security Monitor
+ * Provides `statusTile` attribute for legacy dashboards
+ */
+metadata {
+    definition(name: "LSM Tile Device", namespace: "LSM", author: "WarlockWeary + ChatGPT + Grok") {
+        capability "Actuator"
+        capability "Refresh"
+        attribute "statusTile", "string"
+    }
+}
+def refresh() {
+    log.debug "LSM Tile Device refresh() called"
+}
+def updateTile(txt) {
+    sendEvent(name: "statusTile", value: txt, isStateChange: true)
+}
